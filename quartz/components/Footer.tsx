c023951de1a4b09947670a64a910bd115b87af03
Netkela <seogde@yandex.ru>
2025-06-24 22:16:22 +0300

    vault backup: 2025-06-24 22:16:22

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,10 +13,11 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        /*<p>
+        
+        <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>*/
+          <a href="https://netkela.ru">Netkela</a> Будешь воровать — руки отсохнут © {year}
+        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
