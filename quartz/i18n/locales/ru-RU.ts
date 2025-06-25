a9ecf484e46c01d5ae692d09773126afc6224050
Netkela <seogde@yandex.ru>
2025-06-25 12:33:23 +0300

    vault backup: 2025-06-25 12:33:23

--- quartz/i18n/locales/ru-RU.ts
+++ quartz/i18n/locales/ru-RU.ts
@@ -75,7 +75,7 @@ export default {
     folderContent: {
       folder: "Папка",
       itemsUnderFolder: ({ count }) =>
-        `в этой папке ${count} элемент${getForm(count, "", "а", "ов")}`,
+        `В этой рубрике кол-во статей: ${count} ${getForm(count, "", "а", "ов")}`,
     },
     tagContent: {
       tag: "Тег",
