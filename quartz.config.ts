4bd16bd09d620742a1e1be9b8a7878066e9b8b79
Netkela <seogde@yandex.ru>
2025-06-27 13:22:21 +0300

    vault backup: 2025-06-27 13:22:21

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "netkela.github.io/blog/",
+    baseUrl: "netkela.ru",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
