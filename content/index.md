13bfe9870b3830b0f9a48968cb0375cf8e4b95cc
Netkela <seogde@yandex.ru>
2025-11-03 14:21:39 +0300

    vault backup: 2025-11-03 14:21:39

--- content/index.md
+++ content/index.md
@@ -13,7 +13,7 @@ showBio: false
 dateoff: true
 readoff: true
 ---
-Соло-предприниматель, вебмастер и блогер
+2Соло-предприниматель, вебмастер и блогер
 
 ![[files/site/netkela.jpg]]
 
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -85,7 +85,16 @@ export const defaultContentPageLayout: PageLayout = {
         /* { Component: Component.ReaderMode() }, */
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+  title: "Навигация",
+  folderDefaultState: "collapsed",
+  folderClickBehavior: "link",
+  useSavedState: false,
+  filterFn: (node) => {
+    // Показывать ТОЛЬКО папки, скрыть все файлы
+    return node.isFolder === true
+  },
+})
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
